b25c85a0fd84d252b0b3e5825bcb73ad5111695a
wangsaichao <wangsaichao@ruubypay.com>
2018-08-08 21:36:56 +0800

    cas5.3.2单点登录-单点登出(十一)客户端传统spring配置方式

---
+++ cas-logout-client1/src/main/java/com/wangsaichao/cas/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.wangsaichao.cas.controller;
+
+import com.wangsaichao.cas.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+/**
+ * @author: wangsaichao
+ * @date: 2018/8/1
+ * @description: 用户相关操作controller
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("insert")
+    public String insert(String username){
+
+        userService.insert(username);
+        return "result";
+    }
+
+
+    /**
+     * 跳转到默认页面
+     * @param session
+     * @return
+     */
+    @RequestMapping("/logout1")
+    public String loginOut(HttpSession session){
+        session.invalidate();
+        //这个是直接退出，走的是默认退出方式
+        return "redirect:https://server.cas.com:8443/cas/logout";
+    }
+
+    /**
+     * 跳转到指定页面
+     * @param session
+     * @return
+     */
+    @RequestMapping("/logout2")
+    public String loginOut2(HttpSession session){
+        session.invalidate();
+        //退出登录后，跳转到退成成功的页面，不走默认页面
+        return "redirect:https://server.cas.com:8443/cas/logout?service=http://app1.cas.com:8081";
+    }
+
+}
